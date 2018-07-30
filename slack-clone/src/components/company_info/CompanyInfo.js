0767b23936fb6d89fe678658230cf12954bbc039
Jack C <chen.jack.cs@gmail.com>
2018-07-30 01:37:36 -0400

    fixed the resizing of the landing page by turning into a flex box

--- slack-clone/src/components/company_info/CompanyInfo.js
+++ slack-clone/src/components/company_info/CompanyInfo.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import style from './CompanyInfo.scss';
 import airbnb_logo from './airbnb.png';
 import capitalone_logo from'./capital_one.png';
