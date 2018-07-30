aebc10f1d5434dda4344d388f9b446c18a008903
Jack C <chen.jack.cs@gmail.com>
2018-07-30 00:53:36 -0400

    Added Meta tags for mobile device

--- slack-clone/src/App.js
+++ slack-clone/src/App.js
@@ -5,11 +5,16 @@ import Landing from './components/landing/Landing';
 import CompanyInfo from './components/company_info/CompanyInfo';
 import EmailSignUp from './components/email_signup/EmailSignUp';
 import Footer from './components/footer/Footer';
+import MetaTag from 'react-meta-tags';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <MetaTag>
+          <title> Where work happens | Slack </title>
+          <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        </MetaTag>
         <NavBar/>
         <Notification/>
         <Landing/>
